0525e743c074b96369486ce0be163243f1e9cbd3
IVIX542 <ivix542@gmail.com>
2025-09-26 16:32:23 +0200

    Fix indentation in ListarFicheros.java
    Corrected indentation for variable assignment to improve code readability in the ListarFicheros class.

--- Tarea1-1y1-2/Actividad1.1/ListarFicheros.java
+++ Tarea1-1y1-2/Actividad1.1/ListarFicheros.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 
 public class ListarFicheros {
@@ -6,7 +5,7 @@ public class ListarFicheros {
         //Realiza un programa en Java que utilice el método listFiles() para mostrar la lista de ficheros en un directorio cualquiera, o en un directorio actual.
 
         File directorio = new File("Tarea1-1y1-2"); // Directorio actual
-    File[] ficheros = directorio.listFiles();
+        File[] ficheros = directorio.listFiles();
 
         if (ficheros != null) {
             for (File fichero : ficheros) {
