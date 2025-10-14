a05409b8a1a5de9a988ed888fff9cbdbe5e6dd94
IVIX542 <ivix542@gmail.com>
2025-10-14 17:35:55 +0200

    Pruebas

---
+++ src/Practicando/src/acceso_secuencial/Main.java
@@ -0,0 +1,51 @@
+package acceso_secuencial;
+
+//Utilidades
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+
+//Excepciones
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		//Declaraciones
+		Scanner sc = new Scanner(System.in);
+		String nombre, apellido, nombreDirTrabajo="src//acceso_secuencial//";
+		//Directorio de trabajo
+		File dirTrabajo=new File(nombreDirTrabajo);
+		if(!dirTrabajo.exists()) {
+			dirTrabajo.mkdir();
+			System.out.println("Directorio de trabajo creado.");
+		} else {
+			System.out.println("El directorio ya existe.");
+		}
+		
+		int movil;
+		File fich = new File(nombreDirTrabajo + "Agenda.txt");
+		
+		//Introducir nombre
+		System.out.print("Introduzca su nombre: ");
+		nombre = sc.nextLine();
+		
+		
+		//Introducir apellido
+		System.out.print("\nIntroduzca su primer apellido: ");
+		apellido= sc.nextLine();
+		
+		//Introducir tlf
+		System.out.print("\nIntroduzca su telefono movil: ");
+		movil = sc.nextInt();
+		
+		String contacto = nombre + ", " + apellido + ", " + movil + ".";
+		
+		FileWriter fw = new FileWriter(fich);
+		
+		fw.append("Hola\n");
+		
+	}
+
+}
