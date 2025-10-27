96341c984eb67c41e670e75932352e114b5361a0
IVIX542 <ivix542@gmail.com>
2025-10-27 09:45:26 +0100

    Añadido el método crearXML para generar un archivo XML a partir de los juegos leídos. Se creó el archivo Catalogo_juegos.xml con la estructura adecuada.

--- src/ENTREGABLE_OBLIGATORIO_2_XML_XSL_HTML/Main.java
+++ src/ENTREGABLE_OBLIGATORIO_2_XML_XSL_HTML/Main.java
@@ -10,8 +10,28 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
+
+//XML
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.Source;
+import javax.xml.transform.Result;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+
+//DOM
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
 //Excepciones
 import java.io.IOException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 
 public class Main {
     public static ArrayList<Juego> juegos = new ArrayList<>();
@@ -21,14 +41,20 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        //Declaraciones
         File txt = new File("Catalogo_inicial_sin_formato.txt");
+        File xml = new File("Catalogo_juegos.xml");
+
+        //Llamadas a métodos
         extraerDatos(txt);
+        crearXML(xml);
 
     }
 
     /**
      * Método para extraer los datos del fichero de texto y mostrarlos por consola.
      * @since 1.0
+     * @param txt File fichero de texto a leer
      */
     public static void extraerDatos(File txt){
         try {
@@ -87,10 +113,70 @@ public class Main {
     /**
      * Método para crear un XML a partit de los juegos leídos.
      * @since 1.1
+     * @param xml File fichero XML a crear
      */
-    public static void crearXML(){
+    public static void crearXML(File xml){
         for (Juego juego : juegos) {
-            
+            try {
+                //Declaración de variables relacionadas con el XML
+                DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
+                DocumentBuilder constructor = fabrica.newDocumentBuilder();
+                DOMImplementation implementacion = constructor.getDOMImplementation();
+
+                //Creación del documento XML
+                Document documento = implementacion.createDocument(null, xml.getName(), null);
+                documento.setXmlVersion("1.0");
+
+                //Creación de los elementos principales
+                Element videojuegos = documento.createElement("Videojuegos");
+
+                Element videojuego = documento.createElement("Videojuego");
+                videojuego.setAttribute("id", juego.getId());
+
+                //Elementos de cada videojuego
+                //Titulo
+                Element titulo = documento.createElement("Titulo");
+                Text tetxTitulo = documento.createTextNode(juego.getTitulo());
+                titulo.appendChild(tetxTitulo);
+                videojuego.appendChild(titulo);
+                //Año
+                Element anio = documento.createElement("Año");
+                Text textAnio = documento.createTextNode(juego.getAnio() + "");
+                anio.appendChild(textAnio);
+                videojuego.appendChild(anio);
+                //Compañía
+                Element compania = documento.createElement("Compañía");
+                Text textCompania = documento.createTextNode(juego.getCompania());
+                compania.appendChild(textCompania);
+                videojuego.appendChild(compania);
+                //Consola
+                Element consola = documento.createElement("Consola");
+                Text textConsola = documento.createTextNode(juego.getConsola());
+                consola.appendChild(textConsola);
+                videojuego.appendChild(consola);
+                //Puntuación
+                Element puntuacion = documento.createElement("Puntuación");
+                Text textPuntuacion = documento.createTextNode(juego.getPuntuacion() + "");
+                puntuacion.appendChild(textPuntuacion);
+                videojuego.appendChild(puntuacion);
+
+                videojuegos.appendChild(videojuego);
+                documento.getDocumentElement().appendChild(videojuegos);
+
+                //Configuracion y creacion del fichero XML
+                Source source = new DOMSource(documento);
+                Result result = new StreamResult(xml);
+
+                Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                transformer.transform(source, result);
+
+            } catch (ParserConfigurationException e) {
+                System.out.println("Error al crear el XML: " + e.getMessage());
+            } catch(TransformerConfigurationException e){
+                System.out.println("Error de configuración del transformador: " + e.getMessage());
+            } catch (TransformerException e){
+                System.out.println("Error al transformar el documento XML: " + e.getMessage());
+            }
         }
     }
 }
