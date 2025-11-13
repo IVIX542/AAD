c6422b2835b0e43fa8151a116b0d9cdf562651e1
IVIX542 <ivix542@gmail.com>
2025-11-13 10:35:03 +0100

    Add table selection validation and menu for actions
    Introduced a loop to validate user input when selecting a table, ensuring only existing tables can be chosen. Added a new menuTabla method to display available actions for the selected table and capture user choice.

--- src/sql/src/entregable_obligatorio_conectores/Main.java
+++ src/sql/src/entregable_obligatorio_conectores/Main.java
@@ -13,7 +13,8 @@ public class Main {
 	
 	//Variables globales
 	private final static String SALUDO = "Bienvenido al Gestor de Bases de Datos de MySQL";
-	private final static ArrayList<String> tablas = new ArrayList<String>(); 
+	private final static ArrayList<String> tablas = new ArrayList<String>();
+	
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -24,7 +25,7 @@ public class Main {
 		final String USUARIO = sc.nextLine();
 		
 		//Pedimos la contraseña de la BBDD
-		System.out.print("\nIntroduce la contraseña para " + USUARIO + ":" );
+		System.out.print("\nIntroduce la contraseña para " + USUARIO + ": " );
 		final String CONTRASENIA = sc.nextLine();
 		
 		//Pedimos la BBDD con la que trabajaremos
@@ -40,10 +41,26 @@ public class Main {
 			
 			mostrarTablas(conexion);
 			
-			System.out.print("Escribe la tabla con la que desearías trabajar:");
-			final String TABLA = sc.nextLine();
+			int i = 0;
+			boolean tablaCorrecta = false;
+			String tabla = "";
 			
+			while(i<tablas.size() && !tablaCorrecta) {
+				
+				System.out.print("Escribe la tabla con la que desearías trabajar: ");
+				tabla = sc.nextLine().toLowerCase();
+				
+				if(!tabla.equals(tablas.get(i).toLowerCase())) {
+					System.out.println("Tabla incorrecta, intente de nuevo.");
+				} else {
+					tablaCorrecta = true;
+				}
+				i++;
+			}
 			
+			System.out.println("Tabla " + tabla + " seleccionada.");
+			
+			menuTabla(sc);
 			
 			conexion.close();
 		} catch (SQLException e) {
@@ -76,5 +93,26 @@ public class Main {
 		}
 	}
 	
+	
+	/**
+	 * Método que muestra un menú de elección para realizar acciones sobre una tabla de la BBDD
+	 * @param Scanner sc utilizado para la entrada por teclado
+	 * @return int eleccion de la acción a realizar
+	 * */
+	public static int menuTabla(Scanner sc) {
+		
+		int eleccion = Integer.parseInt(sc.nextLine());
+		
+		System.out.println("Teclea la opción que deseas realizar:");
+		System.out.println("\n1. Ver todos los registros de la tabla");
+		System.out.println("2. Ver el nombre de todas las columnas de la tabla");
+		System.out.println("3. Insertar nuevo registro en la tabla");
+		System.out.println("4. Modificar un registro existente en la tabla");
+		System.out.println("5. Borrar un registro existente");
+		System.out.println("6. Ver el tipo de datos y extensión de las columnas de la tabla");
+		System.out.println("7. Salir del programa");
+		
+		return eleccion;
+	}
 
 }
